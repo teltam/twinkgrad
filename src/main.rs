407dd3baf2ad930d441a0a30ba28c5f3c0f1401a
teltam <telt.yellow.tail@gmail.com>
2024-05-20 22:51:04 -0700

    wow

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use value::Value;
 fn main() {
     // let a = Value { data: 10, _grad: 0.0, };
     let a = Value::new_v(10.);
-    println!("{:?}", a);
+    println!("{}", a.data);
 }
 
 #[cfg(test)]
@@ -21,21 +21,93 @@ mod tests {
 
         let exp = a * b + c / d;
         assert_eq!(exp.data, -149.33333);
+
+        let a = Value::new_v(10.);
+        let b = Value::new_v(-15.);
+
+        let exp = a + b;
+        assert_eq!(exp.data, -5.);
+        assert_eq!(exp._op, "+".to_string());
     }
 
     #[test]
     fn test_eq() {
-        let child = Value::new_v(10.);
-        let a = Value { data: 3., _grad: 0., _prev: vec![ child.clone() ]};
-        let b = Value { data: 3., _grad: 0., _prev: vec![ child.clone() ]};
+        let em_op = "".to_string();
 
-        assert_eq!(a, b);
+        let child1 = Value::new_v(10.);
+        let child2 = Value::new_v(10.);
+        let a = Value::new_op(3., vec![child1], em_op.clone());
+        let b = Value::new_op(3., vec![child2], em_op.clone());
 
-        let child1 = Value::new_v(7.);
-        let child2 = Value::new_v(8.);
-        let a = Value { data: 3., _grad: 0., _prev: vec![ child1 ]};
-        let b = Value { data: 3., _grad: 0., _prev: vec![ child2 ]};
+        assert_eq!(a.data, b.data);
 
-        assert_ne!(a, b);
+        // let child1 = Value::new_v(7.);
+        // let child2 = Value::new_v(8.);
+        // let a = Value { data: 3., _grad: 0., _prev: vec![ child1 ], _op: em_op.clone()};
+        // let b = Value { data: 3., _grad: 0., _prev: vec![ child2 ], _op: em_op.clone()};
+        // assert_ne!(a, b);
+
+        // let child1 = Value::new_v(8.);
+        // let child2 = Value::new_v(8.);
+        // let a = Value { data: 3., _grad: 0., _prev: vec![ child1 ], _op: em_op.clone()};
+        // let b = Value { data: 3., _grad: 0., _prev: vec![ child2 ], _op: em_op.clone()};
+        // this won't work for now but let's assume it does.
+        // assert_ne!(a, b);
+    }
+
+    #[test]
+    fn neuron() {
+        let x1 = Value::new_v(2.0);
+        let x2 = Value::new_v(0.0);
+
+        let w1 = Value::new_v(-3.0);
+        let w2 = Value::new_v(1.0);
+
+        let b = Value::new_v(8.0);
+
+        let l1 = x1*w1;
+        let l2 = x2*w2;
+
+        let la = l1 + l2;
+
+        let n = la + b;
+
+        let o = n.tanh();
+
+        assert_eq!(o.data, 0.9640129);
+    }
+
+    #[test]
+    fn nueron_grad() {
+        let x1 = Value::new_v(2.0);
+        let x2 = Value::new_v(0.0);
+
+        let w1 = Value::new_v(-3.0);
+        let w2 = Value::new_v(1.0);
+
+        let b = Value::new_v(6.8813735870195432);
+
+        let l1 = x1*w1;
+        let l2 = x2*w2;
+
+        let la = l1 + l2;
+
+        let mut n = la + b;
+
+        // let mut o = n.tanh();
+
+        // assert_eq!(o.data, 0.707061);
+
+
+        // o._grad = 1.;
+        //
+        // match o._backward {
+        //     f1  => {}
+        // }
+        // assert_eq!(o.data, 1.)
+
+        n._grad = 1.;
+        (n._backward)();
+        assert_eq!(n.data, 1.);
     }
 }
--- src/value.rs
+++ src/value.rs
@@ -1,35 +1,53 @@
 use std::ops::{Add, Div, Mul, Sub};
+use std::rc::Rc;
+use std::borrow::Borrow;
+use std::mem::ManuallyDrop;
 
-#[derive(Clone, Debug, Default)]
-pub struct Value<T> {
+#[derive(Clone)]
+pub struct Value<T: Copy>
+{
     pub data: T,
-    pub _grad: f32,
+    pub _grad: T,
 
     pub _prev: Vec<Value<T>>,
+    pub _op: String,
+
+    // pub _backward: Rc<dyn FnMut(Value<T>, Value<T>) -> ()>,
+    pub _backward: ManuallyDrop<Rc<dyn FnMut() -> ()>>,
 }
 
 #[allow(dead_code)]
 impl Value<f32> {
     pub fn new() -> Self {
-        Default::default()
+        return Value {
+            data: 0., _grad: 0., _prev: vec![],
+            _op: "".to_string(),
+            _backward: ManuallyDrop::new(Rc::new(|| {})),
+        }
     }
 
     pub fn new_v(val: f32) -> Self {
-        let mut v: Value<f32> = Default::default();
+        let mut v: Value<f32> = Value::new();
         v.data = val;
         v
     }
 
-    pub fn _new_op(val: f32, _children: Vec<Value<f32>>) -> Self {
-        let mut v: Value<f32> = Default::default();
+    pub fn new_op(val: f32, _children: Vec<Value<f32>>, _op: String) -> Self {
+        let mut v: Value<f32> = Value::new();
         v.data = val;
         v._prev = _children;
+        v._op = _op;
         v
     }
 
-
-    pub fn backward(self) -> () {
-        todo!();
+    pub fn clone(&self) -> Value<f32> {
+        Value {
+            data: self.data,
+            _grad: self._grad,
+            _prev: vec![],
+            _op: "".to_string(),
+            _backward: ManuallyDrop::new(Rc::new(|| {})), // Placeholder for backward function
+        }
     }
 }
 
@@ -37,15 +55,10 @@ impl Value<f32> {
 // Operations
 impl PartialEq for Value<f32> {
     fn eq(&self, other: &Self) -> bool {
+        // TODO this is broken until I figure the best way to cmp two ASTs with self-ref.
 
         if self.data != other.data {
             return false;
-        }
-
-        for prev in &self._prev {
-            if !other._prev.contains(prev) {
-                return false;
-            }
         }
 
         return true;
@@ -58,36 +71,117 @@ impl PartialEq for Value<f32> {
 
 impl Value<f32> {
     pub fn to_pow(self, rhs: Value<f32>) -> Value<f32> {
-        return Value::new_v(f32::powf(self.data, rhs.data));
+        let mut nodes = Vec::new();
+
+        let a = self.data;
+        let b = rhs.data;
+        nodes.push(self);
+        nodes.push(rhs);
+
+        return Value::new_op(
+            f32::powf(a, b),
+            nodes,
+            "**".to_string(),
+        );
+    }
+
+    pub fn tanh(mut self) -> Value<f32> {
+        let n = self.data;
+        let t = (f32::powf(2.718, 2. * n) - 1.) / (f32::powf(2.718, 2. * n) + 1.);
+
+        let mut nodes = vec!(self);
+
+        let mut out = Value::new_op(
+            t,
+            nodes.clone(),
+            "tanh".to_string(),
+        );
+
+        out._backward = ManuallyDrop::new (Rc::new(|| {
+            nodes[0]._grad = (1. - f32::powf(t, 2.)) * out._grad;
+        }));
+
+        return out;
     }
 }
+
 
 impl Add for Value<f32> {
     type Output = Value<f32>;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        // return Value::new_v(self.data + rhs.data);
+    fn add(mut self, mut rhs: Self) -> Self::Output {
         let mut nodes = Vec::new();
-        nodes.push(self.clone());
-        return Value::_new_op(
-            self.data + rhs.data,
-            nodes
+
+        let a = self.data;
+        let b = rhs.data;
+        nodes.push(self);
+        nodes.push(rhs);
+
+        let mut out = Value::new_op(
+            a + b,
+            nodes,
+            "+".to_string(),
         );
+
+        out._backward = ManuallyDrop::new(Rc::new(|| {
+            self._grad = 1. * out._grad;
+            rhs._grad = 1. * out._grad;
+        }));
+
+        return out;
     }
 }
 
 impl Sub for Value<f32> {
     type Output = Value<f32>;
 
-    fn sub(self, rhs: Self) -> Self::Output {
-        return Value::new_v(self.data - rhs.data);
+    fn sub(mut self, mut rhs: Self) -> Self::Output {
+        let mut nodes = Vec::new();
+
+        let a = self.data;
+        let b = rhs.data;
+
+        nodes.push(self);
+        nodes.push(rhs);
+
+        let mut out = Value::new_op(
+            a - b,
+            nodes,
+            "-".to_string(),
+        );
+
+        out._backward = ManuallyDrop::new(Rc::new(|| {
+            self._grad = 1. * out._grad;
+            rhs._grad = -1. * out._grad;
+        }));
+
+        return out;
     }}
 
 impl Mul for Value<f32> {
     type Output = Value<f32>;
 
-    fn mul(self, rhs: Self) -> Self::Output {
-        return Value::new_v(self.data * rhs.data);
+    fn mul(mut self, mut rhs: Self) -> Self::Output {
+        let mut nodes = Vec::new();
+
+        let a = self.data;
+        let b = rhs.data;
+
+        nodes.push(self);
+        nodes.push(rhs);
+
+        let mut out = Value::new_op(
+            a * b,
+            nodes,
+            "*".to_string(),
+        );
+
+        out._backward = ManuallyDrop::new(Rc::new(|| {
+            self._grad = b * out._grad;
+            rhs._grad = a * out._grad;
+        }));
+
+        return out;
     }
 }
 
@@ -95,6 +189,18 @@ impl Div for Value<f32> {
     type Output = Value<f32>;
 
     fn div(self, rhs: Self) -> Self::Output {
-        return Value::new_v(self.data / rhs.data);
+        let mut nodes = Vec::new();
+
+        let a = self.data;
+        let b = rhs.data;
+
+        nodes.push(self);
+        nodes.push(rhs);
+
+        return Value::new_op(
+            a / b,
+            nodes,
+            "/".to_string(),
+        );
     }
 }
